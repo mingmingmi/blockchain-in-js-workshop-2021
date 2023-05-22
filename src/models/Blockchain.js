b16022b335cb467ad6bca2c30b5a4301aed67af2
mingmingmi <1337870034@qq.com>
2023-05-22 22:25:57 +0800

    Update Blockchain.js

--- src/models/Blockchain.js
+++ src/models/Blockchain.js
@@ -6,16 +6,33 @@ class Blockchain {
       - 创世区块
       - 存储区块的映射
   */
-  constructor() {}
+  constructor(name) {
+    this.name = name
+    this.genesis = null
+    this.blocks = {}
+    if (this.genesis) {
+      this.blocks[this.genesis.hash] = this.genesis
+    }
+  }
 
   // 2. 定义 longestChain 函数
   /* 
     返回当前链中最长的区块信息列表
   */
-  longestChain() {
-
-
-    return []
+  longestChain () {
+    let longestChain = []
+    for (let hash in this.blocks) {
+      let currBlock = this.blocks[hash]
+      let chain = [currBlock]
+      while (currBlock.parentHash != 'root' && currBlock.hash != 'root') {
+        currBlock = currBlock.getPreviousBlock()
+        chain.unshift(currBlock)
+      }
+      if (chain.length > longestChain.length) {
+        longestChain = chain
+      }
+    }
+    return longestChain
   }
 }
 
