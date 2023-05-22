939b0ff2197c753688da4383f7099195549640f3
mingmingmi <1337870034@qq.com>
2023-05-22 22:46:36 +0800

    Create UTXOpool.js

---
+++ src/models/UTXOpool.js
@@ -0,0 +1,26 @@
+import './UTXO.js'
+
+class UTXOPool {
+  constructor(utxos = {}) {
+    this.utxos = utxos
+  }
+  //添加交易函数s
+  /**
+   * 将交易的信息更新至  UTXOPool 中
+   */
+  _addUTXO (utxo) {
+    //根据公钥判断当前utxo池是否为空
+    //如果utxo池不为空 ， 新增该utxo的amount
+    if (this.utxos[utxo.pubKey] != null) {
+      this.utxos[utxo.pubKey] = { amount: this.utxos[utxo.pubKey].amount + utxo.amount }
+      //若该utxo为utxo池第一个utxo，则令该utxo的amount等于交易池amount
+    } else {
+      this.utxos[utxo.pubKey] = { amount: utxo.amount }
+    }
+  }
+  //将当前 UTXO 的副本克隆
+  clone () {
+    return this.utxos
+  }
+}
+export default UTXOPool
