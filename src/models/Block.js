a28044a4f2158c3838d3d7a4517b65ac54768407
mingmingmi <1337870034@qq.com>
2023-05-22 22:25:51 +0800

    Update Block.js

--- src/models/Block.js
+++ src/models/Block.js
@@ -1,9 +1,19 @@
 class Block {
   // 1. 完成构造函数及其参数
   /* 构造函数需要包含
-
+   -名字
+   -创世区块
+   -存储区块的映射
   */
-  constructor() {}
+  constructor(blockchain, parentHash, height, hash) {
+    this.blockchain = blockchain
+    this.parentHash = parentHash
+    this.height = height
+    this.hash = hash
+  }
+  getPreviousBlock () {
+    return this.blockchain.blocks[this.parentHash]
+  }
 }
 
 export default Block
